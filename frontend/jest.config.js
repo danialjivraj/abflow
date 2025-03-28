82f4c470367f8a157b0c2ffe99573d35fb980027
Danial Jivraj <danialjivraj@gmail.com>
2025-03-28 21:24:58 +0000

    fixed Cannot use 'import.meta' outside a module error

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+export default {
+    testEnvironment: "jsdom",
+    setupFiles: ["<rootDir>/jest.polyfill.js"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    moduleNameMapper: {
+      "\\.(css|less)$": "identity-obj-proxy",
+      "\\.(mp3|jpg|jpeg|png|svg)$": "<rootDir>/__mocks__/fileMock.js"
+    },
+    transform: {
+      "^.+\\.[tj]sx?$": "babel-jest"
+    }
+  };
+  
---
+++ frontend/jest.polyfill.js
@@ -0,0 +1,8 @@
+global.importMeta = {
+    env: {
+      VITE_API_BASE_URL_DEPLOY: "http://localhost:5000"
+    }
+  };
+  
+  process.env.VITE_API_BASE_URL_DEPLOY = "http://localhost:5000";
+  
